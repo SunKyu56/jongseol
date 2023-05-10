c64ecadd6b748302b5ad298817688195f31b825e
SunKyu56 <emfprh123@kookmin.ac.kr>
2023-05-10 22:02:16 +0900

    new split test

--- split_age_data.py
+++ split_age_data.py
@@ -1,5 +1,6 @@
 import os
 import json
+import numpy as np
 
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -13,7 +14,7 @@ class NpEncoder(json.JSONEncoder):
             return super(NpEncoder, self).default(obj)
 
 def get_val_length(array):
-    return round(len(array)*0.2)
+    return round(len(array)*0.1)
 
 path = "/home/student/Downloads/data/UTKFace/imgs/" #나눌 파일의 경로
 file_list = []
@@ -21,29 +22,46 @@ file_list = []
 for filename in os.listdir(path):
     file_list.append(filename)
 
-youth, adult, elder = [], [], []
+youth, student, adult, elder = [], [], [], []
 
 for image in file_list:
-    if image[1] == "_" or int(image[0])*10 + int(image[1]) < 20:
+    temp = image.split('_')
+    if int(temp[0]) <= 15:
         youth.append(image)
-    elif image[2] == "_" and int(image[0])*10 + int(image[1]) >= 20 and int(image[0])*10 + int(image[1]) <= 40:
+    elif int(temp[0]) > 15 and int(temp[0]) <= 25:
+        student.append(image)
+    elif int(temp[0]) > 25 and int(temp[0]) <= 50:
         adult.append(image)
     else:
         elder.append(image)
 
+print(len(youth),len(student),len(adult),len(elder))
+print(student[1000])
+
 val_youth_num = get_val_length(youth)
-train_youth_num = len(youth) - val_youth_num
+test_youth_num = val_youth_num
+train_youth_num = len(youth) - 2 * val_youth_num
+
+val_student_num = get_val_length(student)
+test_student_num = val_student_num
+train_student_num = len(student) - 2 * val_student_num
 
 val_adult_num = get_val_length(adult)
-train_adult_num = len(adult) - val_adult_num
+test_adult_num = val_adult_num
+train_adult_num = len(adult) - 2 * val_adult_num
 
 val_elder_num = get_val_length(elder)
-train_elder_num = len(elder) - val_elder_num
+test_elder_num = val_elder_num
+train_elder_num = len(elder) - 2 * val_elder_num
 
-train_youth, train_adult, train_elder, val_youth, val_adult, val_elder = [], [], [], [], [], []
+train_youth, train_adult, train_elder, train_student, val_youth, val_adult, val_elder, val_student= [], [], [], [], [], [], [], []
+test_youth, test_student, test_adult, test_elder = [], [], [], []
+
 
 for i in range(train_youth_num):
     train_youth.append(youth[i])
+for i in range(train_student_num):
+    train_student.append(student[i])
 for i in range(train_adult_num):
     train_adult.append(adult[i])
 for i in range(train_elder_num):
@@ -51,10 +69,21 @@ for i in range(train_elder_num):
     
 for j in range(val_youth_num):
     val_youth.append(youth[j+train_youth_num])
+for j in range(val_student_num):
+    val_student.append(student[j+train_student_num])
 for j in range(val_adult_num):
     val_adult.append(adult[j+train_adult_num])
 for j in range(val_elder_num):
     val_elder.append(elder[j+train_elder_num])
+
+for k in range(test_youth_num):
+    test_youth.append(youth[k+train_youth_num+val_youth_num])
+for k in range(test_student_num):
+    test_student.append(student[k+train_student_num+val_student_num])
+for k in range(test_adult_num):
+    test_adult.append(adult[k+train_adult_num+val_adult_num])
+for k in range(test_elder_num):
+    test_elder.append(elder[k+train_elder_num+val_elder_num])
 
 final_path = "data/UTK-Face/imgs/"
 
@@ -62,13 +91,16 @@ f = open("data/UTK-Face/age/train.json","w")
 l = []
 
 for i in range(train_youth_num):
-    d = {'img':final_path + train_youth[i], 'label':'0'}
+    d = {'img':final_path + train_youth[i], 'label':0}
+    l.append(d)
+for i in range(train_student_num):
+    d = {'img':final_path + train_student[i], 'label':1}
     l.append(d)
 for i in range(train_adult_num):
-    d = {'img':final_path + train_adult[i], 'label':'1'}
+    d = {'img':final_path + train_adult[i], 'label':2}
     l.append(d)
 for i in range(train_elder_num):
-    d = {'img':final_path + train_elder[i], 'label':'2'}
+    d = {'img':final_path + train_elder[i], 'label':3}
     l.append(d)
     
 json.dump(l,f,indent='\t',cls=NpEncoder)
@@ -78,13 +110,35 @@ f = open("data/UTK-Face/age/val.json","w")
 l = []
 
 for i in range(val_youth_num):
-    d = {'img':final_path + val_youth[i], 'label':'0'}
+    d = {'img':final_path + val_youth[i], 'label':0}
+    l.append(d)
+for i in range(val_student_num):
+    d = {'img':final_path + val_student[i], 'label':1}
     l.append(d)
 for i in range(val_adult_num):
-    d = {'img':final_path + val_adult[i], 'label':'1'}
+    d = {'img':final_path + val_adult[i], 'label':2}
     l.append(d)
 for i in range(val_elder_num):
-    d = {'img':final_path + val_elder[i], 'label':'2'}
+    d = {'img':final_path + val_elder[i], 'label':3}
+    l.append(d)
+
+json.dump(l,f,indent='\t',cls=NpEncoder)
+f.close()
+
+f = open("data/UTK-Face/age/test.json","w")
+l = []
+
+for i in range(val_youth_num):
+    d = {'img':final_path + test_youth[i], 'label':0}
+    l.append(d)
+for i in range(val_student_num):
+    d = {'img':final_path + test_student[i], 'label':1}
+    l.append(d)
+for i in range(val_adult_num):
+    d = {'img':final_path + test_adult[i], 'label':2}
+    l.append(d)
+for i in range(val_elder_num):
+    d = {'img':final_path + test_elder[i], 'label':3}
     l.append(d)
 
 json.dump(l,f,indent='\t',cls=NpEncoder)
--- split_gender_data.py
+++ split_gender_data.py
@@ -1,5 +1,6 @@
 import os
 import json
+import numpy as np
 
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -13,7 +14,7 @@ class NpEncoder(json.JSONEncoder):
             return super(NpEncoder, self).default(obj)
 
 def get_val_length(array):
-    return round(len(array)*0.2)
+    return round(len(array)*0.1)
 
 path = "/home/student/Downloads/data/UTKFace/imgs/" #나눌 파일의 경로
 file_list = []
@@ -24,7 +25,8 @@ for filename in os.listdir(path):
 male, female = [], []
 
 for image in file_list:
-    if "_0_" in image:
+    temp = image.split('_')
+    if temp[1] == '0':
         male.append(image)
     else:
         female.append(image)
@@ -32,10 +34,16 @@ for image in file_list:
 val_male_num = get_val_length(male)
 val_female_num = get_val_length(female)
 
-train_male_num = len(male) - val_male_num
-train_female_num = len(female) - val_female_num
+test_male_num = val_male_num
+test_female_num = val_female_num
+
+train_male_num = len(male) - val_male_num - test_male_num
+train_female_num = len(female) - val_female_num - test_female_num
+
+print(test_female_num, test_male_num)
 
 train_male, train_female, val_male, val_female = [], [], [], []
+test_male, test_female = [], []
 
 for i in range(train_male_num):
     train_male.append(male[i])
@@ -43,13 +51,17 @@ for i in range(train_female_num):
     train_female.append(female[i])
 
 for j in range(val_male_num):
-    val_male.append(male[j+train_male_num])
+    val_male.append(male[j+i])
 for j in range(val_female_num):
-    val_female.append(female[j+train_female_num])
+    val_female.append(female[j+i])
     
+for k in range(test_male_num+1):
+    test_male.append(male[k+j+i])
+for k in range(test_female_num+1):
+    test_female.append(female[k+j+i])
 
-print(len(male) + len(female))
-print(len(train_female) + len(train_male) + len(val_female) + len(val_male))
+print(len(male),len(female))
+print(len(train_female) + len(train_male) + len(val_female) + len(val_male) + len(test_female) + len(test_male))
 
 final_path ="data/UTK-Face/imgs/"
 
@@ -57,10 +69,10 @@ f = open("data/UTK-Face/gender/train.json", "w")
 l = []
 
 for i in range(train_male_num):
-    d = {'img':final_path + train_male[i], 'label':'0'}
+    d = {'img':final_path + train_male[i], 'label':0}
     l.append(d)
 for i in range(train_female_num):
-    d = {'img':final_path + train_female[i], 'label':'1'}
+    d = {'img':final_path + train_female[i], 'label':1}
     l.append(d)
 
 json.dump(l,f,indent='\t',cls=NpEncoder)
@@ -70,10 +82,23 @@ f = open("data/UTK-Face/gender/val.json", "w")
 l = []
 
 for i in range(val_male_num):
-    d = {'img':final_path + val_male[i], 'label':'0'}
+    d = {'img':final_path + val_male[i], 'label':0}
     l.append(d)
 for i in range(val_female_num):
-    d = {'img':final_path + val_female[i], 'label':'1'}
+    d = {'img':final_path + val_female[i], 'label':1}
+    l.append(d)
+
+json.dump(l,f,indent='\t',cls=NpEncoder)
+f.close()
+
+f = open("data/UTK-Face/gender/test.json", "w")
+l = []
+
+for i in range(test_male_num):
+    d = {'img':final_path + test_male[i], 'label':0}
+    l.append(d)
+for i in range(test_female_num):
+    d = {'img':final_path + test_female[i], 'label':1}
     l.append(d)
 
 json.dump(l,f,indent='\t',cls=NpEncoder)
